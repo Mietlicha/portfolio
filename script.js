37c629d2325732be719d8e19321e921abdb25727
Anna Kubiak <ainakubiak@gmail.com>
2020-01-21 15:41:39 +0100

    project page - redy on mobile

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function counting() {
 }
 
 window.addEventListener('load', counting);
+
+// Fetch all details element
+const details = Array.from(document.querySelectorAll('details'));
+
+// Add onclick listeners
+details.forEach(targetDetail => {
+  targetDetail.addEventListener('click', () => {
+    // Close all details that are not targetDetail
+    details.forEach(detail => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute('open');
+      }
+    });
+  });
+});
